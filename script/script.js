b5f7d25f9475844a8ddfd7dc09ca534835f5e0a6
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-03-26 14:51:52 +0100

    Add variables for user input and function to handle input

--- script/script.js
+++ script/script.js
@@ -13,3 +13,24 @@ function multipy(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case value: add;
+            add(firstNumber, secondNumber);
+            break;
+        case value: subtract;
+            subtract(firstNumber, secondNumber);
+            break;
+        case value: multipy
+            multipy(firstNumber, secondNumber);
+            break;
+        case value: divide
+            divide(firstNumber, secondNumber);
+            break;
+    }
+}
